2a77e89a1a3d7b3c92ccf5b4ef97235dff85c686
deannariddlespur <deanna.riddlespur@gmail.com>
2025-07-31 10:35:45 -0600

    Revert "Improve admin interface styling and redirect to custom admin dashboard"
    This reverts commit d49e80f68b2f5779d5002f9f42aa9dac5856a9a3.

--- first-project-1/dogboarding/urls.py
+++ first-project-1/dogboarding/urls.py
@@ -18,14 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import redirect
-
-def redirect_to_custom_admin(request):
-    """Redirect from default admin to custom admin dashboard"""
-    return redirect('/admin-dashboard/')
 
 urlpatterns = [
-    path('admin/', redirect_to_custom_admin, name='admin_redirect'),
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
