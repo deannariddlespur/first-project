ca10d18f32c22a67be0d4bd8b501938f5d80ed82
deannariddlespur <deanna.riddlespur@gmail.com>
2025-08-04 11:21:31 -0600

    Add custom admin classes to exclude problematic fields

--- first-project-1/core/admin.py
+++ first-project-1/core/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import Owner, Dog, Kennel, Booking, DailyLog, Payment, StaffNote, FacilityAvailability
 
-# Standard Django admin registrations - no custom styling
-# Note: User model is already registered by Django's auth admin
+# Custom admin classes to handle missing fields
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'breed', 'age', 'size', 'owner')
+    list_filter = ('size', 'owner')
+    search_fields = ('name', 'breed', 'owner__user__username')
+    readonly_fields = ('id',)
+
+class DailyLogAdmin(admin.ModelAdmin):
+    list_display = ('booking', 'date', 'feeding', 'medication', 'exercise')
+    list_filter = ('date', 'booking__dog__name')
+    search_fields = ('booking__dog__name', 'notes')
+    readonly_fields = ('id',)
+
+# Standard Django admin registrations with custom admin classes
 admin.site.register(Owner)
-admin.site.register(Dog)
+admin.site.register(Dog, DogAdmin)
 admin.site.register(Kennel)
 admin.site.register(Booking)
-admin.site.register(DailyLog)
+admin.site.register(DailyLog, DailyLogAdmin)
 admin.site.register(Payment)
 admin.site.register(StaffNote)
 admin.site.register(FacilityAvailability)
