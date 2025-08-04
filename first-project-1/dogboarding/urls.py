f3d3f7df40d1bebac834bd8d8091de89b257b226
deannariddlespur <deanna.riddlespur@gmail.com>
2025-08-04 09:30:40 -0600

    Simplify to standard Django admin - remove all custom styling

--- first-project-1/dogboarding/urls.py
+++ first-project-1/dogboarding/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.admin import dogboarding_admin
 
-# Only use custom admin site - no default admin
+# Use standard Django admin - no custom styling
 urlpatterns = [
-    path('admin/', dogboarding_admin.urls),
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
