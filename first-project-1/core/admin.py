78f41a5b41d19ba65767214c67368e9b49fd6dfb
deannariddlespur <deanna.riddlespur@gmail.com>
2025-08-04 08:36:42 -0600

    Fix NameError by adding back admin import

--- first-project-1/core/admin.py
+++ first-project-1/core/admin.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import User
 from .models import Owner, Dog, Kennel, Booking, DailyLog, Payment, StaffNote, FacilityAvailability
